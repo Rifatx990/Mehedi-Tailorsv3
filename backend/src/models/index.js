4f6f024ee3a03b239294645643d319a480cbd735
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-12-18 07:45:14 +0600

    Create index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,11 @@
+const User = require('./User');
+const Product = require('./Product');
+const Order = require('./Order');
+const Measurement = require('./Measurement');
+
+module.exports = {
+  User,
+  Product,
+  Order,
+  Measurement,
+};
