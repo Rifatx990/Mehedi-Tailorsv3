51f8e7e86ad2252065fa6be1df1ac62fdfb29621
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-12-18 09:44:09 +0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'; // If you have this
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/css/custom.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
